462841a1d35cb97b1320a1c53515fc6d90599a5a
loxlouro <55482145+loxlouro@users.noreply.github.com>
2024-03-10 18:53:41 +0300

    Update AccountService.kt

--- src/main/kotlin/com/reappa/rieltorapp/services/AccountService.kt
+++ src/main/kotlin/com/reappa/rieltorapp/services/AccountService.kt
@@ -1,6 +1,5 @@
 package com.reappa.rieltorapp.services
 
-
 import com.reappa.rieltorapp.models.Account
 import com.reappa.rieltorapp.models.Role
 import com.reappa.rieltorapp.repo.AccountRepository
@@ -14,15 +13,11 @@ import org.springframework.stereotype.Service
 
 @Service
 @RequiredArgsConstructor
-//@EnableJpaRepositories
 class AccountService (
     @Autowired
     val accountRepository: AccountRepository,
     ):UserDetailsService{
-//        @Bean
-//        fun accountRepository(): AccountRepository{
-//            return accountRepository
-//        }
+    
     @Transactional
     override fun loadUserByUsername(email: String): UserDetails {
      val account: Account? = accountRepository.findByAccountEmail(email)
@@ -31,29 +26,26 @@ class AccountService (
         };
         throw UsernameNotFoundException("Account not found (email: $email")
     }
+    
     fun findAccountByEmail(email: String): Account? {
-
         val account: Account? = accountRepository.findByAccountEmail(email)
-
         if (account != null) {
             return account
         }
         return null
     }
+    
     @Transactional
     fun saveNewAccount(
         email: String,
-        encodedPassword:String,
+        encodedPassword: String,
         role: Role,
         ): Account {
         val accountFromDB: Account? = accountRepository.findByAccountEmail(email)
-
         if (accountFromDB!=null) {
             throw RuntimeException("Email is already registered")
         }
-
-        val account = Account(null,email, encodedPassword
-            , listOf(role))
+        val account = Account(null, email, encodedPassword, listOf(role))
         return accountRepository.save(account)
     }
 }
