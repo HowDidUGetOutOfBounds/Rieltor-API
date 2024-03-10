3c83381a902962c0fdcb97d92fa9b4343cdc75f8
loxlouro <55482145+loxlouro@users.noreply.github.com>
2024-03-10 18:51:58 +0300

    Update AccountRepository.kt

--- src/main/kotlin/com/reappa/rieltorapp/repo/AccountRepository.kt
+++ src/main/kotlin/com/reappa/rieltorapp/repo/AccountRepository.kt
@@ -4,7 +4,6 @@ import com.reappa.rieltorapp.models.Account
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-
 @Repository
 interface AccountRepository :JpaRepository<Account, Long> {
     fun findByAccountEmail(email: String): Account?
