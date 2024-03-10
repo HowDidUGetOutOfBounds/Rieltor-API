9fa3879728ecd0a276524fd0549542327daadbf0
loxlouro <55482145+loxlouro@users.noreply.github.com>
2024-03-10 18:52:12 +0300

    Update RoleRepository.kt

--- src/main/kotlin/com/reappa/rieltorapp/repo/RoleRepository.kt
+++ src/main/kotlin/com/reappa/rieltorapp/repo/RoleRepository.kt
@@ -4,7 +4,6 @@ import com.reappa.rieltorapp.models.Role
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-
 @Repository
 interface RoleRepository : JpaRepository<Role, Long>{
     fun findByRoleName(roleName: String): Role?
