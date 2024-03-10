94a06f8f01bc2c32e5303115d74dc8415718b253
loxlouro <55482145+loxlouro@users.noreply.github.com>
2024-03-10 18:51:43 +0300

    Update Role.kt

--- src/main/kotlin/com/reappa/rieltorapp/models/Role.kt
+++ src/main/kotlin/com/reappa/rieltorapp/models/Role.kt
@@ -9,6 +9,7 @@ import lombok.Getter
 import lombok.RequiredArgsConstructor
 import lombok.Setter
 import org.springframework.security.core.GrantedAuthority
+
 @Entity
 @Data
 @Getter
@@ -17,12 +18,9 @@ import org.springframework.security.core.GrantedAuthority
 data class Role(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @get:JvmName("getRoleId")
     val id: Long?,
-//    @get:JvmName("getRoleName")
     val roleName: String,
 ):GrantedAuthority {
-
     override fun getAuthority(): String {
         return roleName
     }
